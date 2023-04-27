5a779ee7307347d0b6f7c78373c2974168265fc2
yerim8373 <yerim8373@naver.com>
2023-04-28 04:08:29 +0900

    [Baekjoon] 14218 그래프 탐색2 문제풀이ing

---
+++ baekjoon/silver1_14218_그래프탐색2/silver1_14218_그래프탐색2.java
@@ -0,0 +1,40 @@
+package BAEKJOON;
+
+import java.io.*;
+import java.util.*;
+
+public class silver1_14218_그래프탐색2 {
+
+	static ArrayList<Integer>[] list;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		list = new ArrayList[n+1];
+		
+		for (int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		int q = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
+		}
+	}
+
+}
